94facb5c705d6ffe7e2e5229591d5ab0a61efea5
Thomas N <Quetzacoalt91@users.noreply.github.com>
2016-07-12 15:34:27 +0200

    Update get-module.sh

--- 1.6/config_files/get-module.sh
+++ 1.6/config_files/get-module.sh
@@ -3,6 +3,6 @@
 echo "\n* Requesting module $ID_MODULE ...";
 curl "$GET_FILE_MODULE$ID_MODULE" >> /var/www/html/modules/module.zip
 echo "\n* Unzipping the module ...";
-unzip -q module.zip -d /var/www/html/modules/
+unzip -q /var/www/html/modules/module.zip -d /var/www/html/modules/
 rm /var/www/html/modules/module.zip
 
