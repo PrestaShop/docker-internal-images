5175766b2c98dd7b73aa2db6c8ee538fe962cf67
faje <favreaujean@live.fr>
2016-07-12 10:07:47 +0000

    [+] Shuffle: Add module download

--- 1.6/config_files/docker-customization_run.sh
+++ 1.6/config_files/docker-customization_run.sh
@@ -7,9 +7,13 @@ echo "\n* Updating PrestaShop domains ...";
 req='UPDATE ps_configuration SET value = "'$PS_DOMAIN'" WHERE name IN ("PS_SHOP_DOMAIN", "PS_SHOP_DOMAIN_SSL"); UPDATE ps_shop_url SET domain = "'$PS_DOMAIN'", domain_ssl = "'$PS_DOMAIN'";'
 mysql -h $DB_SERVER -P $DB_PORT -u $DB_USER -p$DB_PASSWD -D$DB_NAME -e "${req}"
 
-if [ $PS_ADDONS_MODULE -ne 0 ]; then
-	echo "\n* Requesting module $PS_ADDONS_MODULE ...";
-	# Insert code to execute here
+if [ $ID_MODULE -ne 0 ]; then
+	echo "\n* Requesting module $ID_MODULE ...";
+	curl "$GET_FILE_MODULE$ID_MODULE" >> /var/www/html/modules/module.zip
+	echo "Unzip the module";
+	cd /var/www/html/modules/
+	unzip -q module.zip
+	cd /var/www/html
 fi
 
 bash /tmp/docker_run.sh
