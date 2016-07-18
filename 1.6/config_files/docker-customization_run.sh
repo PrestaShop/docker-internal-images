08b05afdd29ae143bd606294922615fb134c58a0
Thomas N <Quetzacoalt91@users.noreply.github.com>
2016-07-18 11:52:21 +0200

    Update test of non-empty value

--- 1.6/config_files/docker-customization_run.sh
+++ 1.6/config_files/docker-customization_run.sh
@@ -7,7 +7,7 @@ echo "\n* Updating PrestaShop domains ...";
 req='UPDATE ps_configuration SET value = "'$PS_DOMAIN'" WHERE name IN ("PS_SHOP_DOMAIN", "PS_SHOP_DOMAIN_SSL"); UPDATE ps_shop_url SET domain = "'$PS_DOMAIN'", domain_ssl = "'$PS_DOMAIN'";'
 mysql -h $DB_SERVER -P $DB_PORT -u $DB_USER -p$DB_PASSWD -D$DB_NAME -e "${req}"
 
-if [ $ID_MODULE -ne 0 ]; then
+if [ -n "$ID_MODULE" ]; then
 	echo "\n* Requesting module $ID_MODULE ...";
 	bash /tmp/get-module.sh&
 fi
