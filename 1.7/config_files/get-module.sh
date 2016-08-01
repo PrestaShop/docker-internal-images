246962ce802e7d444d0290eb406ec751d59804f2
Thomas Nabord <thomas.nabord@prestashop.com>
2016-08-01 17:06:49 +0200

    ADD PS 1.7 and fix module env vars

---
+++ 1.7/config_files/get-module.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "\n* Requesting module $ID_MODULE ...";
+curl "$GET_FILE_MODULE$ID_MODULE" >> /var/www/html/modules/module.zip
+echo "\n* Unzipping the module ...";
+unzip -q /var/www/html/modules/module.zip -d /var/www/html/modules/
+rm /var/www/html/modules/module.zip
+
