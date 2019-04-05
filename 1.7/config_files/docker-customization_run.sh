f328207014f5efd7a017a7332ab05024a3c0b55a
Thomas Nabord <thomas.nabord@prestashop.com>
2019-04-05 17:43:05 +0100

    Run all pre-run tasks in background

--- 1.7/config_files/docker-customization_run.sh
+++ 1.7/config_files/docker-customization_run.sh
@@ -5,8 +5,8 @@ service mysql start
 
 echo "\n* Updating PrestaShop domains ...";
 req='UPDATE ps_configuration SET value = "'$PS_DOMAIN'" WHERE name IN ("PS_SHOP_DOMAIN", "PS_SHOP_DOMAIN_SSL"); UPDATE ps_shop_url SET domain = "'$PS_DOMAIN'", domain_ssl = "'$PS_DOMAIN'";'
-mysql -h$DB_SERVER -P$DB_PORT -u$DB_USER -p$DB_PASSWD -D$DB_NAME -e "${req}"
-php /tmp/update-domain.php
+mysql -h$DB_SERVER -P$DB_PORT -u$DB_USER -p$DB_PASSWD -D$DB_NAME -e "${req}" &
+php /tmp/update-domain.php &
 
 if [ "$ID_MODULE" != "0" ]; then
 	echo "\n* Requesting module $ID_MODULE ...";
