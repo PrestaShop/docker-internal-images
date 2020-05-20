792a745d55ea1e12e1ae8df2be1a5ba53c4ff134
zalexki <haumalex@gmail.com>
2020-05-20 12:49:52 +0200

    add support of PS_ENABLE_SSL

--- nightly/config_files/docker-customization_run.sh
+++ nightly/config_files/docker-customization_run.sh
@@ -6,6 +6,10 @@ service mysql start
 echo "\n* Updating PrestaShop domains ...";
 req='UPDATE ps_configuration SET value = "'$PS_DOMAIN'" WHERE name IN ("PS_SHOP_DOMAIN", "PS_SHOP_DOMAIN_SSL"); UPDATE ps_shop_url SET domain = "'$PS_DOMAIN'", domain_ssl = "'$PS_DOMAIN'";'
 mysql -h$DB_SERVER -P$DB_PORT -u$DB_USER -p$DB_PASSWD -D$DB_NAME -e "${req}" &
+
+req='UPDATE ps_configuration SET value = "'$PS_ENABLE_SSL'" WHERE name IN ("PS_SSL_ENABLED", "PS_SSL_ENABLED_EVERYWHERE");'
+mysql -h$DB_SERVER -P$DB_PORT -u$DB_USER -p$DB_PASSWD -D$DB_NAME -e "${req}" &
+
 php /tmp/update-domain.php &
 
 if [ "$ID_MODULE" != "0" ]; then
